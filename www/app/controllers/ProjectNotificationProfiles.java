a6a78ee97e42ee83c5d8a4f6b1c3fa6585b8f969
Mahmoud Sakr <kamasheto@users.noreply.github.com>
2010-06-20 15:20:05 +0000

    Issue 2 revisited

--- www/app/controllers/ProjectNotificationProfiles.java
+++ www/app/controllers/ProjectNotificationProfiles.java
@@ -4,5 +4,23 @@ import play.mvc.With;
 
 @With (Secure.class)
 public class ProjectNotificationProfiles extends SmartCRUD {
+	public static void show(String id) {
+		forbidden();
+	}
 
+	public static void delete(String id) {
+		forbidden();
+	}
+
+	public static void blank() {
+		forbidden();
+	}
+
+	public static void create() {
+		forbidden();
+	}
+
+	public static void save(String id) {
+		forbidden();
+	}
 }
