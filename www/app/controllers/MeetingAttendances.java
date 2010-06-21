dd1aeec4c0e7537bf3d0dfc97799431fd48c3f54
amorotto <amorotto@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-06-21 02:33:00 +0000

    Start Issue 18
    1) added permission in bootstrap (setMeetingAttendance)
    2) added 3 methods in meetingAttendance.java
    3) added the setAttendance page
    4) a link of set attendance page put in the viewMeeting.html page

--- www/app/controllers/MeetingAttendances.java
+++ www/app/controllers/MeetingAttendances.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import models.Meeting;
 import models.MeetingAttendance;
 import models.Project;
 import models.User;
 import play.mvc.With;
 
-@With (Secure.class)
-public class MeetingAttendances extends SmartCRUD {
+@With( Secure.class )
+public class MeetingAttendances extends SmartCRUD
+{
 
 	/**
 	 * this method takes meetingHash as a parameter and set the status of the
@@ -24,26 +26,28 @@ public class MeetingAttendances extends SmartCRUD {
 	 * @param meetingHash
 	 */
 
-	public static void confirm(String meetingHash) {
-		MeetingAttendance attendance = MeetingAttendance.find("byMeetingHash", meetingHash).first();
+	public static void confirm( String meetingHash )
+	{
+		MeetingAttendance attendance = MeetingAttendance.find( "byMeetingHash", meetingHash ).first();
 		String status = attendance.status;
 		Date currentDate = new Date();
-		Date tempStart = new Date(attendance.meeting.startTime);
-		boolean notYet = tempStart.after(currentDate);
+		Date tempStart = new Date( attendance.meeting.startTime );
+		boolean notYet = tempStart.after( currentDate );
 		boolean setbefore = false;
 		User user = Security.getConnected();
 		boolean isUser = false;
-		if (user.id == attendance.user.id)
+		if( user.id == attendance.user.id )
 			isUser = true;
-		if (!status.equals("waiting")) {
+		if( !status.equals( "waiting" ) )
+		{
 			setbefore = true;
 			attendance.save();
-			render(attendance, setbefore, notYet, isUser);
+			render( attendance, setbefore, notYet, isUser );
 		}
 		attendance.status = "confirmed";
-		Logs.addLog(Security.getConnected(), "confirm", "Meeting invitation", attendance.id, attendance.meeting.project, new Date(System.currentTimeMillis()));
+		Logs.addLog( Security.getConnected(), "confirm", "Meeting invitation", attendance.id, attendance.meeting.project, new Date( System.currentTimeMillis() ) );
 		attendance.save();
-		render(attendance, setbefore, notYet, isUser);
+		render( attendance, setbefore, notYet, isUser );
 
 	}
 
@@ -59,54 +63,62 @@ public class MeetingAttendances extends SmartCRUD {
 	 * @param meetingHash
 	 */
 
-	public static void decline(String meetingHash) {
-		MeetingAttendance attendance = MeetingAttendance.find("byMeetingHash", meetingHash).first();
+	public static void decline( String meetingHash )
+	{
+		MeetingAttendance attendance = MeetingAttendance.find( "byMeetingHash", meetingHash ).first();
 		String status = attendance.status;
 		Date currentDate = new Date();
-		Date tempStart = new Date(attendance.meeting.startTime);
-		boolean notYet = tempStart.after(currentDate);
+		Date tempStart = new Date( attendance.meeting.startTime );
+		boolean notYet = tempStart.after( currentDate );
 		boolean setbefore = false;
 		User user = Security.getConnected();
 		boolean isUser = false;
 
-		if (user.id == attendance.user.id)
+		if( user.id == attendance.user.id )
 			isUser = true;
 
-		if (!status.equals("waiting")) {
+		if( !status.equals( "waiting" ) )
+		{
 			setbefore = true;
-			render(attendance, setbefore, notYet, isUser);
+			render( attendance, setbefore, notYet, isUser );
 			return;
 		}
 		attendance.status = "declined";
-		Logs.addLog(Security.getConnected(), "decline", "Meeting invitation", attendance.id, attendance.meeting.project, new Date(System.currentTimeMillis()));
+		Logs.addLog( Security.getConnected(), "decline", "Meeting invitation", attendance.id, attendance.meeting.project, new Date( System.currentTimeMillis() ) );
 		attendance.save();
-		if (notYet) {
+		if( notYet )
+		{
 
 			boolean flag = true;
-			List<MeetingAttendance> attendees = MeetingAttendance.find("byMeeting.id", attendance.meeting.id).fetch();
-			while (attendees.isEmpty() == false) {
-				MeetingAttendance temp = (MeetingAttendance) attendees.remove(0);
+			List<MeetingAttendance> attendees = MeetingAttendance.find( "byMeeting.id", attendance.meeting.id ).fetch();
+			while( attendees.isEmpty() == false )
+			{
+				MeetingAttendance temp = (MeetingAttendance) attendees.remove( 0 );
 				String tempStatus = temp.status;
-				if (tempStatus.equals("confirmed") || tempStatus.equals("waiting"))
+				if( tempStatus.equals( "confirmed" ) || tempStatus.equals( "waiting" ) )
 					flag = false;
 			}
-			if (flag == true) {
+			if( flag == true )
+			{
 
 				attendance.meeting.status = false;
 				attendance.meeting.save();
 				attendance.save();
-				attendees = MeetingAttendance.find("byMeeting.id", attendance.meeting.id).fetch();
+				attendees = MeetingAttendance.find( "byMeeting.id", attendance.meeting.id ).fetch();
 				List<User> users = new ArrayList<User>();
-				while (attendees.isEmpty() == false) {
-					users.add(attendees.remove(0).user);
+				while( attendees.isEmpty() == false )
+				{
+					users.add( attendees.remove( 0 ).user );
 				}
-				Notifications.notifyUsers(users, "Meeting Canceled", "All attendees declined invitations", (byte) -1);
+				Notifications.notifyUsers( users, "Meeting Canceled", "All attendees declined invitations", (byte) -1 );
 			}
 
-			render(attendance, notYet, setbefore, isUser);
+			render( attendance, notYet, setbefore, isUser );
 
-		} else {
-			render(attendance, notYet, setbefore, isUser);
+		}
+		else
+		{
+			render( attendance, notYet, setbefore, isUser );
 		}
 
 	}
@@ -120,8 +132,9 @@ public class MeetingAttendances extends SmartCRUD {
 	 * @param excuse
 	 */
 
-	public static void setExcuse(String meetingHash, String excuse) {
-		MeetingAttendance attendance = MeetingAttendance.find("byMeetingHash", meetingHash).first();
+	public static void setExcuse( String meetingHash, String excuse )
+	{
+		MeetingAttendance attendance = MeetingAttendance.find( "byMeetingHash", meetingHash ).first();
 		attendance.reason = excuse;
 		attendance.save();
 
@@ -134,20 +147,77 @@ public class MeetingAttendances extends SmartCRUD {
 	 * @param projectID
 	 * @param userID
 	 */
-	public static void viewInvites(long projectID) {
+	public static void viewInvites( long projectID )
+	{
 		long userID = Security.getConnected().id;
-		User currentUser = User.findById(userID);
-		Project currentProject = Project.findById(projectID);
-		List<MeetingAttendance> allmeetings = MeetingAttendance.find("byUser.idAndMeeting.project.idAndDeletedAndStatusLike", userID, projectID, false, "waiting").fetch();
+		User currentUser = User.findById( userID );
+		Project currentProject = Project.findById( projectID );
+		List<MeetingAttendance> allmeetings = MeetingAttendance.find( "byUser.idAndMeeting.project.idAndDeletedAndStatusLike", userID, projectID, false, "waiting" ).fetch();
 		List<MeetingAttendance> meetings = new ArrayList<MeetingAttendance>();
 		Date currentDate = new Date();
-		for (MeetingAttendance meeting : allmeetings) {
-			Date meetingDate = new Date(meeting.meeting.startTime);
-			if (currentDate.before(meetingDate)) {
-				meetings.add(meeting);
+		for( MeetingAttendance meeting : allmeetings )
+		{
+			Date meetingDate = new Date( meeting.meeting.startTime );
+			if( currentDate.before( meetingDate ) )
+			{
+				meetings.add( meeting );
 			}
 		}
-		render(meetings, currentUser, currentProject);
+		render( meetings, currentUser, currentProject );
+	}
+
+	/**
+	 * Set attendance page that Renders to the user all the meetingAttendances
+	 * associated to that meeting in order to change in the attendance of the
+	 * meeting attendees
+	 * 
+	 * @param meetingID
+	 */
+	public static void setAttendance( long meetingID )
+	{
+		Meeting meeting = Meeting.findById( meetingID );
+		List<MeetingAttendance> attendances = MeetingAttendance.find( "byMeeting.idAndDeleted", meetingID, false ).fetch();
+		render( attendances, meeting );
+	}
+
+	/**
+	 * The method setConfirmed is called from the setAttendance page in order to
+	 * change the status of the user in the given meeting to attnded and send
+	 * him an email to notify the user
+	 * 
+	 * @param id
+	 *            which is the MeetingAttendance id
+	 */
+
+	public static void setConfirmed( long id )
+	{
+		MeetingAttendance ma = MeetingAttendance.findById( id );
+		ma.status = "confirmed";
+		ma.save();
+		String header = "Attendance to " + ma.meeting.name;
+		String body = "Dear " + ma.user.getDisplayName( ma.meeting.project ) + "\n";
+		String body2 = "Your attendance to " + ma.meeting.name + " was confirmed.";
+		Notifications.notifyUsers( ma.user, header, body + body2, (byte) 1 );
+	}
+
+	/**
+	 * The method setDeclined is called from the setAttendance page in order to
+	 * change the status of the user in the given meeting to did not attend and
+	 * send him an email to notify the user
+	 * 
+	 * @param id
+	 *            which is the MeetingAttendance id
+	 */
+	public static void setDeclined( long id, String reason )
+	{
+		MeetingAttendance ma = MeetingAttendance.findById( id );
+		ma.status = "declined";
+		ma.reason = reason;
+		ma.save();
+		String header = "Attendance to " + ma.meeting.name;
+		String body = "Dear " + ma.user.getDisplayName( ma.meeting.project ) + "\n";
+		String body2 = "Your attendance to " + ma.meeting.name + " was changed to NOT attended.";
+		Notifications.notifyUsers( ma.user, header, body + body2, (byte) 1 );
 	}
 
 }
