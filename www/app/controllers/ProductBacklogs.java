664477e390c6e130cccce69b802613a27d4504c0
iistcrimi <iistcrimi@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-06-21 21:00:50 +0000

    the show graph method was updated

--- www/app/controllers/ProductBacklogs.java
+++ www/app/controllers/ProductBacklogs.java
@@ -122,13 +122,24 @@ public class ProductBacklogs extends SmartController {
 		Component myComponent = Component.findById( componentId );
 		String Data = temp.fetchData( componentId );
 		List<Sprint> SprintsInProject = temp.sprints;
+		int maxDays =0;
 		for(int i =0; i<SprintsInProject.size(); i++)
 		{
 			if(SprintsInProject.get(i).tasks.size()==0)
 				SprintsInProject.remove( i );
+			else
+			{
+				for(int j =0; j<SprintsInProject.size();j++)
+				{
+					if(SprintsInProject.get( j ).getDuration()>=SprintsInProject.get( i ).getDuration())
+						maxDays= SprintsInProject.get( j ).getDuration();
+				}
+			}
 		}
 		if (Data.startsWith("GenerateFullGraph([[[]]"))
 			Data = null;
+		else
+			Data = Data.substring( 0,18 )+maxDays+","+Data.substring( 18 );
 		render(Data, SprintsInProject, temp, componentId, myComponent);
 	}
 }
