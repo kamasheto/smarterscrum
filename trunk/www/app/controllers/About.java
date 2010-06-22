e2366cba00b0c66983ea80f6b599a5cc99abc253
me@sakr.me <me@sakr.me@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-06-22 17:59:47 +0000

    Added more information about the developers
    Update issue 63
    Status: started

--- trunk/www/app/controllers/About.java
+++ trunk/www/app/controllers/About.java
@@ -8,13 +8,13 @@ public class About extends SmartController {
 		"Mahmoud Sharawi (C3)<br />", // 
 		"Joseph ElHajj (C5)<br />Joe speaks Japanese, and watches cartoon.<br />(Even during meetings!)", // 
 		"Hossam Sharaf (C2)<br />", // 
-		"Mohamed Monayri (C3)<br />", // 
+		"Mohamed Monayri (C3)<br />One of the people behind the estimation game!", // 
 		"Moataz Mekki (C1)<br />Moataz is notifying everyone!", // 
 		"Omar Nabil (C1)<br />", // 
 		"Hossam Amer (C4)<br />Hossam was working on the review log.<br />We still don't know what that is.<br />Just kidding. We do. Really. (Just don't ask.)", // 
 		"Amr Othman (C1)<br />Wallas logs everything.", // 
 		"Moumen Mohamed (C3)<br />Moumen took care of task types.<br />He's also very tall. Really really tall.", // 
-		"Galal Aly (C3-lead)<br />Galal mesh moktane3. Period.", // 
+		"Galal Aly (C3-lead)<br />Galal is also one of the people behind the estimation game.<br /> He's also mesh moktane3.", // 
 		"Ahmed Behairy (C2)<br />He approves project requests. Really.", // 
 		"Saher (scrum-master)<br />He made sure we followed the rules.<br />He also brought cake. Lots and lots of it.", // 
 		"Amr Abdelwahab (C5)<br />Amr took snapshots. During meetings, after sprints.", // 
