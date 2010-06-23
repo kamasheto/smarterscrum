facb1910496157a34f7d84192f3dddd30e1cc601
amorotto <amorotto@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-06-23 19:06:18 +0000

    added sessions.getOnline(long id) that will be used to get the project online users in the chat system

--- trunk/www/app/controllers/Sessions.java
+++ trunk/www/app/controllers/Sessions.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import models.Project;
 import models.Session;
 import models.User;
 import play.mvc.With;
@@ -12,21 +13,47 @@ import play.mvc.With;
  * 
  * @author mahmoudsakr
  */
-@With (Secure.class)
-public class Sessions extends SmartController {
+@With( Secure.class )
+public class Sessions extends SmartController
+{
 	/**
 	 * pings the server, updates the session of the current user
 	 */
-	public static void ping() {
+	public static void ping()
+	{
 		Session.update();
 
-		List<Session> sessions = Session.find("order by lastClick desc").fetch();
+		List<Session> sessions = Session.find( "order by lastClick desc" ).fetch();
 
 		List<User.Object> users = new ArrayList<User.Object>();
-		for (Session session : sessions) {
-			users.add(new User.Object(session.user.id, session.user.name, session.lastClick, session.user.isAdmin));
+		for( Session session : sessions )
+		{
+			users.add( new User.Object( session.user.id, session.user.name, session.lastClick, session.user.isAdmin ) );
 		}
-		renderJSON(users);
+		renderJSON( users );
+	}
+
+	/**
+	 * pings the server, updates the online users of a certian project
+	 * 
+	 * @param id
+	 *            (project id)
+	 * @author Amr Hany
+	 */
+
+	public static void getOnline( long id )
+	{
+		Session.update();
+
+		List<Session> sessions = Session.find( "order by lastClick desc" ).fetch();
+
+		List<User.Object> onlineUsers = new ArrayList<User.Object>();
+		for( Session session : sessions )
+		{
+			if( session.user.projects.contains( Project.findById( id ) ) )
+				onlineUsers.add( new User.Object( session.user.id, session.user.name, session.lastClick, session.user.isAdmin ) );
+		}
+		renderJSON( onlineUsers );
 	}
 
 	/**
@@ -35,8 +62,9 @@ public class Sessions extends SmartController {
 	 * 
 	 * @throws Throwable
 	 */
-	public static void logout() throws Throwable {
-		Session.delete("user = ?", Security.getConnected());
+	public static void logout() throws Throwable
+	{
+		Session.delete( "user = ?", Security.getConnected() );
 		Secure.logout();
 	}
 }
