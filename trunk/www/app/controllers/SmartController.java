28de8b8abe7aa472787baa91ebd89a85f95f6da6
amr.mohamed.abdelwahab <amr.mohamed.abdelwahab@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-06-25 13:18:34 +0000

    issue 37, added the method that checks if there is sprint that ended and change its flag to true

--- trunk/www/app/controllers/SmartController.java
+++ trunk/www/app/controllers/SmartController.java
@@ -1,5 +1,12 @@
 package controllers;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import models.Meeting;
+import models.Sprint;
+
 import play.mvc.Before;
 import play.mvc.Controller;
 
@@ -7,5 +14,18 @@ public class SmartController extends Controller {
 	@Before
 	public static void addDefaults() {
 		renderArgs.put("connected", Security.getConnected());
+	}
+	@Before
+	public static void endsprint() {
+		List<Sprint> sprints=Sprint.findAll();
+		for (Sprint s : sprints) {
+			Date now = Calendar.getInstance().getTime();
+			if( s.endDate.before( now ) )
+			{
+				s.ended = true;
+	s.save();
+			}
+			
+		}
 	}
 }
