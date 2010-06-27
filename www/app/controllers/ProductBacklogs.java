c81712ca1b2d5218742e7cacadd7a465f2db377d
iistcrimi <iistcrimi@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-06-27 19:10:38 +0000

    the security was updated

--- www/app/controllers/ProductBacklogs.java
+++ www/app/controllers/ProductBacklogs.java
@@ -93,7 +93,7 @@ public class ProductBacklogs extends SmartController {
 
 		} else {
 			Component component = Component.findById(id);
-			Security.check(component.project.users.contains(Security.getConnected()));
+			Security.check(component.componentUsers.contains( Security.getConnected() ));
 			name = component.name;
 			Long compId = component.id;
 			List<List<Story>> stories = new LinkedList<List<Story>>();
