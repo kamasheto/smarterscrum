5a76d57ca6fdf932c348b7d062e885e729221d45
Mahmoud Sakr <kamasheto@users.noreply.github.com>
2010-06-29 15:10:56 +0000

    Projects now are initialized with roles set by system admins dynamically instead of hardcoded in the init of Projects
    Update issue 128
    Status: Pending

--- www/app/controllers/SmartController.java
+++ www/app/controllers/SmartController.java
@@ -4,28 +4,29 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-import models.Meeting;
 import models.Sprint;
-
 import play.mvc.Before;
 import play.mvc.Controller;
 
 public class SmartController extends Controller {
 	@Before
-	public static void addDefaults() {
+	public static void addDefaults() throws Throwable {
 		renderArgs.put("connected", Security.getConnected());
+		if (Security.isConnected() && Security.getConnected().deleted) {
+			Secure.logout();
+		}
 	}
+
 	@Before
 	public static void endsprint() {
-		List<Sprint> sprints=Sprint.findAll();
+		List<Sprint> sprints = Sprint.findAll();
 		for (Sprint s : sprints) {
 			Date now = Calendar.getInstance().getTime();
-			if( s.endDate.before( now ) )
-			{
+			if (s.endDate.before(now)) {
 				s.ended = true;
-	s.save();
+				s.save();
 			}
-			
+
 		}
 	}
 }
