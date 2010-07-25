83444b0dcbb607d864fc4ec291d6d09be7cea4e3
iistcrimi@gmail.com <iistcrimi@gmail.com@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-07-25 16:29:22 +0000

    update issue 222 status: pending Now, not all the nested content is loaded. When dragged out the content is automatically loaded. A few bugs were fixed.

--- trunk/www/public/javascripts/functions.js
+++ trunk/www/public/javascripts/functions.js
@@ -1,5 +1,7 @@
 ﻿$(function() {
 	$('.draggable').live('mouseover', function() {
+
+			$(this).data('init',1);
 			$(this).draggable({handle: '.ui-widget-header',cancel: 'img'});
 			var h = $(this).height();
 			var w = $(this).width();
@@ -9,77 +11,82 @@
 				minWidth: w,
 				autoHide: true
 			});
+		
 	});
 
 	
-		$(".draggableChild").live('mouseover',function(){
+	$(".draggableChild").live('mouseover',function(){
+		
+			$(this).draggable({handle: '.ui-widget-header',cancel: 'img',stop: function(event, ui){
+				var x = $(this).attr('class').split(" ");
+				var is = false;
+				for(var i =0; i<x.length;i++)
+				{
+					if(x[i]=='draggableChild')
+					is = true;
+				}
+				if(is)
+				{
+					var el = $(this);
+					var pos = el.position();
+					var pos2 = el.closest('.draggable').position();
+	    			el.css({
+	    				position: "absolute",
+	    				marginLeft: 0,
+	    				marginTop: 0,
+	    				top: pos2.top+pos.top, 
+	    				left: pos2.left+pos.left 
+	    			});		
+					$("#container").append($(this));
+				}
+				$(this).draggable({start:function(event,ui){},stop:function(event,ui){}});
+				$(this).children().show();
+				$(this).removeClass('draggableChild');
+				$(this).addClass('draggable');
+				if(!$(this).data('loaded')&&$(this).find('.ui-widget-content').first().html()=='')
+				{
+					load($(this).attr('name')+' .actual',$(this).attr('id'));
+					$(this).data('loaded',9);
+				}
 	
-		$(this).draggable({handle: '.ui-widget-header',cancel: 'img',stop: function(event, ui){
-			var x = $(this).attr('class').split(" ");
-			var is = false;
-			for(var i =0; i<x.length;i++)
-			{
-				if(x[i]=='draggableChild')
-				is = true;
-			}
-			if(is)
-			{
-				var el = $(this);
-				var pos = el.position();
-				var pos2 = el.closest('.draggable').position();
-    			el.css({
-    				position: "absolute",
-    				marginLeft: 0,
-    				marginTop: 0,
-    				top: pos2.top+pos.top, 
-    				left: pos2.left+pos.left 
-    			});		
-				$("#container").append($(this));
-			}
-			//alert($(this).find('.ui-widget-content').children());
-			//if($(this).find('.ui-widget-content').children().first().attr('class')=='loading')
-			//{//alert('');
-				//$(this).find('.ui-widget-content').first().load($(this).attr('name'));
-			//}
-			$(this).draggable({start:function(event,ui){},stop:function(event,ui){}});
-			$(this).children().show();
-			$(this).removeClass('draggableChild');
-			$(this).addClass('draggable');
-
-		},
-		start:function(event,ui){
-			//alert('');
-			var id= $(this).attr('id');
-			var t = $('#'+id+' .ui-widget-header').first().clone();
-			$(t).css('background','#333');
-			$(t).addClass('clone');
-			$(t).insertBefore($(this));
-			$(t).attr('id',$(this).attr('id')+"_2");
-		}});
-	
-	
+			},
+			start:function(event,ui){
+				var id= $(this).attr('id');
+				var t = $('#'+id+' .ui-widget-header').first().clone();
+				$(t).css('background','#333');
+				$(t).addClass('clone');
+				$(t).insertBefore($(this));
+				$(t).attr('id',$(this).attr('id')+"_2");
+			}});
+		
 	});
 
 
 	$('.min').live('click',function(){
-		$(this).parent().next().slideToggle();
+		if(!$(this).parent().parent().data('loaded')&&$(this).parent().next().html()=='')
+		{
+			load($(this).parent().parent().attr('name')+' .actual',$(this).parent().parent().attr('id'));
+			$(this).parent().parent().data('loaded',9);
+		}else
+			$(this).parent().next().slideToggle();
+		
 	});
 	
 	$('.revertFrom').live('click',function(){
 		var theId =  $(this).parent().parent().attr('id');
 		var theSecondId = theId+"_2";
-		//alert($('#'+theId).attr('id'));
 		if($('#'+theSecondId).attr('id')==null)
 			$('#'+theId).remove();
 		else
 		{
-		$('#'+theId).removeAttr('style');
-		$('#'+theId).removeClass('draggable');
-		$('#'+theId).addClass('draggableChild');
-		$('#'+theSecondId).replaceWith($('#'+theId));
+			$('#'+theId).removeAttr('style');
+			$('#'+theId).removeClass('draggable');
+			$('#'+theId).addClass('draggableChild');
+			$('#'+theSecondId).replaceWith($('#'+theId));
 		}
 	});
 });
+
 function load(url,el)
 {
 	$('#'+el+'_content').slideDown(400);
@@ -92,12 +99,10 @@ function load(url,el)
 	});
 }
 
-
 function loadBox(url,el)
 {
 	$('#container').load(url,function(){
 		$('#'+el+' .loading').first().hide();
-	//	alert($('#'+el+'_content').find('.loading').first().attr('class'));
 	});
 }
 
