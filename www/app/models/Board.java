b2fc6dc86dcc3a790ddbd7ce0efa2d91704c0043
dina.e.helal <dina.e.helal@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-07-26 09:21:03 +0000

    corrected small mistake in the component constructor & removed some commented code

--- www/app/models/Board.java
+++ www/app/models/Board.java
@@ -10,14 +10,10 @@ import javax.persistence.OneToOne;
 @Entity
 public class Board extends SmartModel {
 
-	// public ArrayList<ArrayList<ArrayList<String>>> data;
 	@OneToMany (mappedBy = "board")
 	public List<Snapshot> snapshot;
 	@OneToMany (mappedBy = "board")
 	public List<Column> columns;
-
-	// @OneToMany (mappedBy = "board")
-	// public List<HistoryBoard> historyBoard;
 
 	@OneToOne
 	public Project project;
@@ -28,8 +24,6 @@ public class Board extends SmartModel {
 	public String name;
 	public boolean deleted;
 
-	// public String[] data;
-
 	public Board (Project project) {
 		this();
 		this.project = project;
@@ -37,7 +31,7 @@ public class Board extends SmartModel {
 
 	public Board (Component component2) {
 		this();
-		component = component;
+		component = component2;
 	}
 
 	public Board () {
