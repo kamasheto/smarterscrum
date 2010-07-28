f6e71fad4903c577d2a332977edfb2dd6a7c7c51
iistcrimi <iistcrimi@gmail.com>
2010-07-28 06:29:28 +0000

    Project/blank() was updated to match the overlay. blank.html was update likewise

--- www/app/controllers/Projects.java
+++ www/app/controllers/Projects.java
@@ -78,10 +78,12 @@ public class Projects extends SmartCRUD {
 			if (Security.getConnected().isAdmin) {
 
 				flash.success("' " + projectObject.name + " '" + " Project Has Been Successfully Created.");
-				redirect(request.controller + ".show", object.getEntityId());
+				//redirect(request.controller + ".show", object.getEntityId());
+				redirect("Application/overlayKiller");
 			} else {
 				flash.success("Your Project Request Has Been Sent.You Will Be Notified Upon Approval");
-				redirect("/show/projects");
+			//	redirect("/show/projects");
+				redirect("Application/overlayKiller");
 			}
 		}
 	}
