cdeb7042d8e5896366caf40fd8a9bc94fbf37b70
galalaly28@gmail.com <galalaly28@gmail.com@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-07-29 08:16:12 +0000

    update issue 214 Added the comments controller Added method deleteComment(long id)

---
+++ trunk/www/app/controllers/Comments.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import models.Comment;
+import play.mvc.With;
+
+@With (Secure.class)
+public class Comments extends SmartController {
+	
+	public static void deleteComment(long id){
+		Comment comment = Comment.findById(id);
+		if(comment == null)
+			renderText("Comment not found.");
+		else
+		{
+			comment.delete();
+			renderText("Comment deleted successfully.");
+		}
+	}
+}
