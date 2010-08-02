5b8d433302130e89305f479db6f6c2fcac8140ae
amorotto <amorotto@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-08-02 06:22:11 +0000

    fixing bug

--- trunk/www/app/controllers/Requests.java
+++ trunk/www/app/controllers/Requests.java
@@ -3,18 +3,18 @@ package controllers;
 import java.util.Date;
 import java.util.List;
 
-import notifiers.Notifications;
-
 import models.Component;
 import models.Project;
 import models.Request;
 import models.Role;
 import models.User;
 import models.UserNotificationProfile;
+import notifiers.Notifications;
 import play.mvc.With;
 
-@With (Secure.class)
-public class Requests extends SmartCRUD {
+@With( Secure.class )
+public class Requests extends SmartCRUD
+{
 	/**
 	 * belongs to s15
 	 * <p>
@@ -40,12 +40,13 @@ public class Requests extends SmartCRUD {
 	 * @Task C1S14
 	 */
 	// @Check ("canManageRequests")
-	public static void requestRespond(long id) {
-		Project pro = Project.findById(id);
-		Security.check(pro, "manageRequests");
-		List<Request> requests = Request.find("isDeletion = false and project = " + pro.id + " order by id desc").fetch();
-		List<Request> drequests = Request.find("isDeletion = true and project = " + pro.id + " order by id desc").fetch();
-		render(requests, drequests, pro);
+	public static void requestRespond( long id )
+	{
+		Project pro = Project.findById( id );
+		Security.check( pro, "manageRequests" );
+		List<Request> requests = Request.find( "isDeletion = false and project = " + pro.id + " order by id desc" ).fetch();
+		List<Request> drequests = Request.find( "isDeletion = true and project = " + pro.id + " order by id desc" ).fetch();
+		render( requests, drequests, pro );
 	}
 
 	/**
@@ -59,13 +60,14 @@ public class Requests extends SmartCRUD {
 	 * @param hash
 	 *            : to find the request that was accepted
 	 */
-	public static void requestAccept(String hash) {
-		Request x = Request.find("byHash", hash).first();
+	public static void requestAccept( String hash )
+	{
+		Request x = Request.find( "byHash", hash ).first();
 		Project y = x.project;
-		x.user.addRole(x.role);
-		Notifications.notifyUsers(x.user, "Role Request Accepted", "Your Role request to be " + x.role.name + " in " + x.role.project.name + " has been accepted", (byte) 1);
+		x.user.addRole( x.role );
+		Notifications.notifyUsers( x.user, "Role Request Accepted", "Your Role request to be " + x.role.name + " in " + x.role.project.name + " has been accepted", (byte) 1 );
 		User myUser = Security.getConnected();
-		Logs.addLog(myUser, "RequestAccept", "Request", x.id, y, new Date());
+		Logs.addLog( myUser, "RequestAccept", "Request", x.id, y, new Date() );
 		x.delete();
 	}
 
@@ -88,36 +90,41 @@ public class Requests extends SmartCRUD {
 	 * @Task C1S14
 	 */
 	// @Check ("canManageRequests")
-	public static void deletionRequestAccept(String hash) throws Throwable {
-		if (hash == null)
+	public static void deletionRequestAccept( String hash ) throws Throwable
+	{
+		if( hash == null )
 			Secure.login();
-		Request currentRequest = Request.find("byHash", hash).first();
-		Security.check(currentRequest.project, "manageRequests");
-		if (currentRequest == null)
+		Request currentRequest = Request.find( "byHash", hash ).first();
+		Security.check( currentRequest.project, "manageRequests" );
+		if( currentRequest == null )
 			Secure.login();
-		UserNotificationProfile currentProfile = UserNotificationProfile.find("user =  " + currentRequest.user.id + " and project = " + currentRequest.project.id).first();
-		if (currentProfile != null)
+		UserNotificationProfile currentProfile = UserNotificationProfile.find( "user =  " + currentRequest.user.id + " and project = " + currentRequest.project.id ).first();
+		if( currentProfile != null )
 			currentProfile.delete();
-		List<Role> projectRoles = Role.find("project", currentRequest.project).fetch();
+		List<Role> projectRoles = Role.find( "project", currentRequest.project ).fetch();
 
-		for (int i = 0; i < projectRoles.size(); i++) {
-			if (currentRequest.user.roles.contains(projectRoles.get(i))) {
-				currentRequest.user.roles.remove(projectRoles.get(i));
+		for( int i = 0; i < projectRoles.size(); i++ )
+		{
+			if( currentRequest.user.roles.contains( projectRoles.get( i ) ) )
+			{
+				currentRequest.user.roles.remove( projectRoles.get( i ) );
 				currentRequest.user.save();
 			}
 		}
 
-		currentRequest.user.projects.remove(currentRequest.project);
+		currentRequest.user.projects.remove( currentRequest.project );
 		currentRequest.user.save();
-		if (!currentRequest.user.components.isEmpty()) {
-			List<Component> currentComponents = Component.find("project", currentRequest.project).fetch();
-			for (int i = 0; i < currentComponents.size(); i++) {
-				currentRequest.user.components.remove(currentComponents.get(i));
+		if( !currentRequest.user.components.isEmpty() )
+		{
+			List<Component> currentComponents = Component.find( "project", currentRequest.project ).fetch();
+			for( int i = 0; i < currentComponents.size(); i++ )
+			{
+				currentRequest.user.components.remove( currentComponents.get( i ) );
 				currentRequest.user.save();
 			}
 		}
-		Notifications.notifyUsers(currentRequest.user, "Project deletion Request Accepted", "Your request to be deleted from " + currentRequest.project.name + " has been accepted", (byte) 1);
-		Logs.addLog(Security.getConnected(), "DeletionRequestAccept", "Request", currentRequest.id, currentRequest.project, new Date());
+		Notifications.notifyUsers( currentRequest.user, "Project deletion Request Accepted", "Your request to be deleted from " + currentRequest.project.name + " has been accepted", (byte) 1 );
+		Logs.addLog( Security.getConnected(), "DeletionRequestAccept", "Request", currentRequest.id, currentRequest.project, new Date() );
 		currentRequest.delete();
 	}
 
@@ -138,17 +145,21 @@ public class Requests extends SmartCRUD {
 	 * @Task C1S14
 	 */
 	// @Check ("canManageRequests")
-	public static void requestIgnore(String hash, String body) throws Throwable {
-		if (hash == null)
+	public static void requestIgnore( String hash, String body ) throws Throwable
+	{
+		if( hash == null )
 			Secure.login();
-		Request x = Request.find("byHash", hash).first();
-		Security.check(x.project, "manageRequests");
-		if (x == null)
+		Request x = Request.find( "byHash", hash ).first();
+		Security.check( x.project, "manageRequests" );
+		if( x == null )
 			Secure.login();
 		Project y = x.project;
-		if (!x.isDeletion) {
-			Notifications.notifyUsers(x.user, "Role Request Denied", "Your Role request to be " + x.role.name + " in " + y.name + " has been denied", (byte) -1);
-		} else {
+		if( !x.deleted )
+		{
+			Notifications.notifyUsers( x.user, "Role Request Denied", "Your Role request to be " + x.role.name + " in " + y.name + " has been denied", (byte) -1 );
+		}
+		else
+		{
 			/*
 			 * if (body == null) Notifications.notifyUsers(x.user,
 			 * "deletion request from project denied",
@@ -160,16 +171,16 @@ public class Requests extends SmartCRUD {
 			 * " has been denied because " + body + ".", (byte) -1);
 			 */
 			{
-				String b = body.replace('+', ' ');
-				int i = body.indexOf('&');
+				String b = body.replace( '+', ' ' );
+				int i = body.indexOf( '&' );
 				i += 6;
-				b = b.substring(i);
-				Notifications.notifyUsers(x.user, "deletion request from project denied", "Your deletion request from project " + x.project.name + " has been denied because " + b + ".", (byte) -1);
+				b = b.substring( i );
+				Notifications.notifyUsers( x.user, "deletion request from project denied", "Your deletion request from project " + x.project.name + " has been denied because " + b + ".", (byte) -1 );
 			}
 		}
 		User myUser = Security.getConnected();
 		Date dd = new Date();
-		Logs.addLog(myUser, "RequestDeny", "Request", x.id, y, dd);
+		Logs.addLog( myUser, "RequestDeny", "Request", x.id, y, dd );
 		x.delete();
 	}
 
@@ -181,62 +192,71 @@ public class Requests extends SmartCRUD {
 	 * @param id
 	 */
 
-	public static void RequestDeleted(long id) {
+	public static void RequestDeleted( long id )
+	{
 
 		User myUser = Security.getConnected();
-		Component myComponent = Component.findById(id);
-		Request x = new Request(myUser, myComponent);
-		flash.success("your request has been sent");
+		Component myComponent = Component.findById( id );
+		Request x = new Request( myUser, myComponent );
+		flash.success( "your request has been sent" );
 		x.save();
-		Show.project(myComponent.project.id);
+		Show.project( myComponent.project.id );
 		// Logs.addLog(myComponent, "request to be deleted", "Request", x.id );
 
 	}
 
-	public static void list() {
-		Security.check(Security.getConnected().isAdmin);
-		List<User> users = User.find("pendingDeletion = true").fetch();
-		render(users);
+	public static void list()
+	{
+		Security.check( Security.getConnected().isAdmin );
+		List<User> users = User.find( "pendingDeletion = true" ).fetch();
+		render( users );
 	}
 
-	public static void deleteUsers(int[] users) {
-		for (int i = 0; i < users.length; i++) {
-			User currentUser = User.find("id = " + users[i]).first();
+	public static void deleteUsers( int[] users )
+	{
+		for( int i = 0; i < users.length; i++ )
+		{
+			User currentUser = User.find( "id = " + users[i] ).first();
 			currentUser.deleted = true;
 			currentUser.pendingDeletion = false;
 			currentUser.save();
-			Notifications.notifyUsers(currentUser, "Your deletion request from our system has been approved.", "Dear " + currentUser.name + ", your deletion request from our system has been approved by a system admin. we hope to see you back again!", (byte) -1);
+			Notifications.notifyUsers( currentUser, "Your deletion request from our system has been approved.", "Dear " + currentUser.name + ", your deletion request from our system has been approved by a system admin. we hope to see you back again!", (byte) -1 );
 		}
-		flash.success("Users are now deactivated ");
-		redirect("/admin/requests");
+		flash.success( "Users are now deactivated " );
+		redirect( "/admin/requests" );
 	}
-	
-	
-	public static void removeRequestRoleInProject(long roleId){
-		Role role = Role.findById(roleId);
+
+	public static void removeRequestRoleInProject( long roleId )
+	{
+		Role role = Role.findById( roleId );
 		User user = Security.getConnected();
-		Request request = Request.find("byRoleAndUser", role, user).first();
+		Request request = Request.find( "byRoleAndUser", role, user ).first();
 		request.delete();
-		renderText("Request removed!");
+		renderText( "Request removed!" );
 	}
-	
-	public static void show() {
+
+	public static void show()
+	{
 		forbidden();
 	}
 
-	public static void save() {
+	public static void save()
+	{
 		forbidden();
 	}
 
-	public static void blank() {
+	public static void blank()
+	{
 		forbidden();
 	}
 
-	public static void create() {
+	public static void create()
+	{
 		forbidden();
 	}
 
-	public static void delete() {
+	public static void delete()
+	{
 		forbidden();
 	}
 
