2dfa6ab8084054175bc74a7dd48bb2c6e13ad5a3
dina.e.helal <dina.e.helal@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-08-04 12:23:12 +0000

    added error message

--- www/app/controllers/Tasks.java
+++ www/app/controllers/Tasks.java
@@ -1902,6 +1902,8 @@ public class Tasks extends SmartCRUD
 		Task task = Task.findById(taskId);
 		User user = User.findById(assigneeId);
 		User connected = Security.getConnected();
+		if(task.reviewer==user)
+			renderText("You can't be the assignee & reviewer of the same task");	
 		Security.check(connected.in(task.project).can("modifyTask") && user.projects.contains(task.project) && task.reviewer!=user && (task.component==null || user.components.contains(task.component)));
 		task.assignee = user;
 		task.save();
@@ -1912,6 +1914,8 @@ public class Tasks extends SmartCRUD
 		Task task = Task.findById(taskId);
 		User user = User.findById(reviewerId);
 		User connected = Security.getConnected();
+		if(task.assignee==user)
+			renderText("You can't be the reviewer & assignee of the same task");	
 		Security.check(connected.in(task.project).can("modifyTask") && user.projects.contains(task.project) && task.assignee!=user && (task.component==null || user.components.contains(task.component)));
 		task.reviewer = user;
 		task.save();
