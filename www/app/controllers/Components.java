5b4f495fe2eb77a6444c5679dcaf36c297f9688a
moataz.mekki <moataz.mekki@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-08-04 18:16:25 +0000

    updated the urls passed to externalOpen

--- www/app/controllers/Components.java
+++ www/app/controllers/Components.java
@@ -75,7 +75,7 @@ public class Components extends SmartCRUD {
 		object.save();
 		temp.init();
 		Logs.addLog(Security.getConnected(), "Create", "Component", temp.id, currentProject, new Date(System.currentTimeMillis()));
-		String url = "@{Application.externalOpen(" + temp.project.id + ", '/components/viewthecomponent?componentId=" + temp.id + "', false)}";
+		String url = "/application/externalopen?id="+temp.project.id+"&isOverlay=false&url=/components/viewthecomponent?componentId="+temp.id;		
 		Notifications.notifyProjectUsers(temp.project, "onCreateComponent", url, "Component", temp.name, (byte) 0);
 		flash.success(Messages.get("crud.created", type.modelName, object.getEntityId()));
 		if (params.get("_save") != null) {
@@ -113,7 +113,7 @@ public class Components extends SmartCRUD {
 		}
 		object.save();
 
-		String url = "@{Application.externalOpen(" + temp.project.id + ", '/components/viewthecomponent?componentId=" + temp.id + "', false)}";
+		String url = "/application/externalopen?id="+temp.project.id+"&isOverlay=false&url=/components/viewthecomponent?componentId="+temp.id;
 		Notifications.notifyProjectUsers(temp.project, "onEditComponent", url, "Component", temp.name, (byte) 0);
 		Logs.addLog(Security.getConnected(), "Edit", "Component", temp.id, temp.project, new Date(System.currentTimeMillis()));
 		flash.success(Messages.get("crud.saved", type.modelName, object.getEntityId()));
@@ -145,7 +145,7 @@ public class Components extends SmartCRUD {
 		try {
 			component.deleteComponent();
 			Logs.addLog(Security.getConnected(), "Delete", "Component", component.id, component.project, new Date(System.currentTimeMillis()));
-			String url = "@{Application.externalOpen(" + component.project.id + ", '/components/listcomponentsinproject?projectId=" + component.project.id + "', false)}";
+			String url = "/application/externalopen?id="+component.project.id+"&isOverlay=false&url=/components/listcomponentsinproject?projectId="+component.project.id;			
 			Notifications.notifyProjectUsers(component.project, "onDeleteComponent", url, "Component", component.name, (byte) -1);
 		} catch (Exception e) {
 			// flash.error(Messages.get("crud.delete.error", type.modelName,
