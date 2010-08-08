34f7fd4395a2db2a0d865496ca533bf518afad69
amr.mohamed.abdelwahab <amr.mohamed.abdelwahab@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-08-08 07:59:13 +0000

    Documentation.

--- trunk/www/app/models/Sprint.java
+++ trunk/www/app/models/Sprint.java
@@ -15,117 +15,177 @@ import javax.persistence.OneToMany;
 import play.data.validation.Required;
 
 @Entity
-public class Sprint extends SmartModel
-{
+public class Sprint extends SmartModel {
+	/**
+	 * The sprint number in this project.
+	 */
 
 	public String sprintNumber;
+	/**
+	 * The sprint's start date.
+	 */
 
 	@Required
 	public Date startDate;
-
+	/**
+	 * The sprint's end date.
+	 */
 	public Date endDate;
-
+	/**
+	 * If true then this sprint is deleted.
+	 */
 	public boolean deleted;
+	/**
+	 * If true this sprint has ended.
+	 */
 	public boolean ended;
 	public Snapshot finalsnapshot;
-
+	/**
+	 * The project of this sprint.
+	 */
 	@Required
 	@ManyToOne
 	public Project project;
-
-	@OneToMany( mappedBy = "sprint", cascade = CascadeType.ALL )
+	/**
+	 * The Meetings associated to this sprint.
+	 */
+	@OneToMany(mappedBy = "sprint", cascade = CascadeType.ALL)
 	public List<Meeting> meetings;
-
-	@OneToMany( mappedBy = "taskSprint", cascade = CascadeType.ALL )
+	/**
+	 * The tasks associated to this sprint.
+	 */
+	@OneToMany(mappedBy = "taskSprint", cascade = CascadeType.ALL)
 	public List<Task> tasks;
 
 	// @OneToMany (mappedBy = "daySprint", cascade = CascadeType.ALL)
 	// public List<Day> days;
-
-	public Sprint( int year, int month, int day, Project p )
-	{
+	/**
+	 * Class constructor initializing (Start date, end date ,project and sprint
+	 * number).
+	 * @param year: year of the end date.
+	 * @param month: month of the end date.
+	 * @param day: day of the end date.
+	 * @param p: project of this sprint
+	 */
+	
+	public Sprint(int year, int month, int day, Project p) {
 		this();
-		startDate = new GregorianCalendar( year, month - 1, day ).getTime();
+		startDate = new GregorianCalendar(year, month - 1, day).getTime();
 		endDate = new GregorianCalendar().getTime();
 		int defaultDays = p.sprintDuration;
-		endDate.setTime( startDate.getTime() + (86400000 * defaultDays) );
+		endDate.setTime(startDate.getTime() + (86400000 * defaultDays));
 		project = p;
 		this.sprintNumber = p.sprints.size() + 1 + "";
-		p.sprints.add( this );
+		p.sprints.add(this);
 		deleted = false;
 	}
 
-	// public Sprint (int year, int month, int day, Project p) {
-	// startDate = new GregorianCalendar(year, month - 1, day).getTime();
-	// endDate = new GregorianCalendar().getTime();
-	// endDate.setTime(startDate.getTime() + (86400000 * 14));
-	// project = p;
-	// deleted = false;
-	// // days = d;
-	// // this.sprintNumber = p.sprints.size() + 1 + "";
-	// }
-
-	public Sprint( int startyear, int startmonth, int startday, int endyear, int endmonth, int endday, Project p )
-	{
+	/**
+	 * Class constructor initializing (Start date, end date ,project and sprint
+	 * number).
+	 * 
+	 * @param startyear: year of the start date.
+	 * @param startmonth: Month of the start date.
+	 * @param startday: day of the start date.
+	 * @param endyear: year of the end date.
+	 * @param endmonth: month of the end date.
+	 * @param endday: day of the end date.
+	 * @param p: project of this sprint
+	 */
+	public Sprint(int startyear, int startmonth, int startday, int endyear,
+			int endmonth, int endday, Project p) {
 		this();
-		startDate = new GregorianCalendar( startyear, startmonth - 1, startday ).getTime();
-		endDate = new GregorianCalendar( endyear, endmonth - 1, endday ).getTime();
+		startDate = new GregorianCalendar(startyear, startmonth - 1, startday)
+				.getTime();
+		endDate = new GregorianCalendar(endyear, endmonth - 1, endday)
+				.getTime();
 		project = p;
-		// this.sprintNumber = p.getSprintCounter();
+
 		this.sprintNumber = p.sprints.size() + 1 + "";
-		p.sprints.add( this );
+		p.sprints.add(this);
 		deleted = false;
 	}
-	
-	public Sprint( Date startDate, Date endDate, Project p )
-	{
+
+	/**
+	 * Class constructor initializing (Start date, end date ,project and sprint
+	 * number).
+	 * 
+	 * @param startDate: start date of the sprint
+	 * @param endDate: end date of the sprint
+	 * @param p: project of the sprint
+	 */
+	public Sprint(Date startDate, Date endDate, Project p) {
 		this();
-		this.startDate = startDate; 
+		this.startDate = startDate;
 		this.endDate = endDate;
 		this.project = p;
 		// this.sprintNumber = p.getSprintCounter();
 		this.sprintNumber = p.sprints.size() + 1 + "";
-		p.sprints.add( this );
+		p.sprints.add(this);
 		deleted = false;
 	}
 
-
-	public Sprint()
-	{
+	/**
+	 *Class constructor initializing (Sprint meetings, and sprint associated
+	 * tasks).
+	 */
+	public Sprint() {
 		meetings = new ArrayList<Meeting>();
 		tasks = new ArrayList<Task>();
 	}
 
-	public boolean setStartDate( int year, int month, int day )
-	{
-		startDate = new GregorianCalendar( year, month--, day ).getTime();
+	/**
+	 * Setter for the start date of the sprint
+	 * 
+	 * @param year
+	 *            : year of the sprint start date
+	 * @param month
+	 *            : month of the sprint start date
+	 * @param day
+	 *            : day of the sprint start date
+	 * 
+	 */
+	public boolean setStartDate(int year, int month, int day) {
+		startDate = new GregorianCalendar(year, month--, day).getTime();
 		return true;
 	}
 
-	public boolean setEndDate( int year, int month, int day )
-	{
-		endDate = new GregorianCalendar( year, month, day ).getTime();
+	/**
+	 * Setter for the end date of the sprint
+	 * 
+	 * @param year
+	 *            : year of the sprint end date
+	 * @param month
+	 *            : month of the sprint end date
+	 * @param day
+	 *            : day of the sprint end date
+	 * 
+	 */
+	public boolean setEndDate(int year, int month, int day) {
+		endDate = new GregorianCalendar(year, month, day).getTime();
 		return true;
 	}
 
-	public int getDuration()
-	{
+	/**
+	 * 
+	 * @return the duration of the sprint
+	 */
+	public int getDuration() {
 		int duration = (int) ((endDate.getTime() - startDate.getTime()) / 86400000);
 		return duration;
 
 	}
 
 	/**
-	 * this method is to format the date and remove the time from it
+	 * formats the date and remove the time from it
 	 * 
 	 * @author minazaki
 	 * @param date
 	 * @return dateformatted
 	 */
-	public String format( Date date )
-	{
+	public String format(Date date) {
 
-		return DateFormat.getDateInstance().format( date );
+		return DateFormat.getDateInstance().format(date);
 	}
 
 	/**
@@ -134,9 +194,8 @@ public class Sprint extends SmartModel
 	 * @param projId
 	 * @return List<Sprint>
 	 */
-	public List<Sprint> getSprints( long projId )
-	{
-		return Sprint.find( "project.id=" + projId ).fetch();
+	public List<Sprint> getSprints(long projId) {
+		return Sprint.find("project.id=" + projId).fetch();
 	}
 
 	/**
@@ -150,48 +209,37 @@ public class Sprint extends SmartModel
 	 * @category C4S9
 	 * @return String of the sprint's data
 	 */
-	public String getCoordinatesOfData( long cid )
-	{
+	public String getCoordinatesOfData(long cid) {
 		List<Task> taskss = new ArrayList<Task>();
-		if( cid == 0 )
-		{
+		if (cid == 0) {
 			taskss = tasks;
-		}
-		else
-		{
-			for( int i = 0; i < tasks.size(); i++ )
-			{
-				if( tasks.get( i ).component.id == cid )
-					taskss.add( tasks.get( i ) );
+		} else {
+			for (int i = 0; i < tasks.size(); i++) {
+				if (tasks.get(i).component.id == cid)
+					taskss.add(tasks.get(i));
 
 			}
 		}
 		int numberOfDays = getDuration();
 		double[] estimationPointsPerDay = new double[numberOfDays];
-		System.out.println( cid + " " + taskss.size() + " " + tasks.size() );
-		if( taskss.size() == 0 )
+		System.out.println(cid + " " + taskss.size() + " " + tasks.size());
+		if (taskss.size() == 0)
 			return "[[]]";
-		for( int i = 0; i < estimationPointsPerDay.length; i++ )
-		{
-			for( int j = 0; j < taskss.size(); j++ )
-			{
-				if( taskss.get( j ).estimationPointsPerDay.size() > i )
-				{
-					estimationPointsPerDay[i] = estimationPointsPerDay[i] + taskss.get( j ).getEffortPerDay( i );
+		for (int i = 0; i < estimationPointsPerDay.length; i++) {
+			for (int j = 0; j < taskss.size(); j++) {
+				if (taskss.get(j).estimationPointsPerDay.size() > i) {
+					estimationPointsPerDay[i] = estimationPointsPerDay[i]
+							+ taskss.get(j).getEffortPerDay(i);
 				}
 			}
 		}
 
 		String m = "[";
 
-		for( int i = 0; i < estimationPointsPerDay.length; i++ )
-		{
-			if( i == estimationPointsPerDay.length - 1 )
-			{
+		for (int i = 0; i < estimationPointsPerDay.length; i++) {
+			if (i == estimationPointsPerDay.length - 1) {
 				m = m + "[" + (i) + "," + estimationPointsPerDay[i] + "]";
-			}
-			else
-			{
+			} else {
 				m = m + "[" + (i) + "," + estimationPointsPerDay[i] + "],";
 			}
 		}
@@ -207,19 +255,17 @@ public class Sprint extends SmartModel
 	 * @story C4S13.
 	 * @description Gets all the Impediment task in a given sprint.
 	 */
-	public static LinkedList<Task> getImpedimentTasks( long Sprint_id )
-	{
-		Sprint temp = Sprint.findById( Sprint_id );
+	public static LinkedList<Task> getImpedimentTasks(long Sprint_id) {
+		Sprint temp = Sprint.findById(Sprint_id);
 
 		List<Task> STasks = temp.tasks;
 		LinkedList<Task> Impediment = new LinkedList<Task>();
 		int j = 0;
-		for( int i = 0; i < STasks.size(); i++ )
-		{
-			Task Current = STasks.get( i );
-			if( Current.taskType != null && Current.taskType.name == "Impediment" )
-			{
-				Impediment.add( j, Current );
+		for (int i = 0; i < STasks.size(); i++) {
+			Task Current = STasks.get(i);
+			if (Current.taskType != null
+					&& Current.taskType.name == "Impediment") {
+				Impediment.add(j, Current);
 				j++;
 			}
 
@@ -239,37 +285,30 @@ public class Sprint extends SmartModel
 	 * @category C4S8 and C4S9
 	 * @return String of the sprint's data
 	 */
-	public String fetchData( long cid )
-	{
+	public String fetchData(long cid) {
 		List<Task> taskss = new ArrayList<Task>();
-		if( cid == 0 )
-		{
+		if (cid == 0) {
 			taskss = tasks;
-		}
-		else
-		{
-			for( int i = 0; i < tasks.size(); i++ )
-			{
-				if( tasks.get( i ).component.id == cid )
-					taskss.add( tasks.get( i ) );
+		} else {
+			for (int i = 0; i < tasks.size(); i++) {
+				if (tasks.get(i).component.id == cid)
+					taskss.add(tasks.get(i));
 
 			}
 		}
 		int numberOfDays = getDuration();
-		System.out.println( numberOfDays );
+		System.out.println(numberOfDays);
 		double[] estimationPointsPerDay = new double[numberOfDays];
 		int yMax = 0;
-		if( taskss.size() == 0 )
+		if (taskss.size() == 0)
 			return "'NONE',[],[],'','','',1,1";
-		for( int i = 0; i < estimationPointsPerDay.length; i++ )
-		{
-			for( int j = 0; j < taskss.size(); j++ )
-			{
-				if( i == 0 )
-					yMax = yMax + (int) (taskss.get( j ).estimationPoints);
-				if( taskss.get( j ).estimationPointsPerDay.size() > i )
-				{
-					estimationPointsPerDay[i] = estimationPointsPerDay[i] + taskss.get( j ).getEffortPerDay( i );
+		for (int i = 0; i < estimationPointsPerDay.length; i++) {
+			for (int j = 0; j < taskss.size(); j++) {
+				if (i == 0)
+					yMax = yMax + (int) (taskss.get(j).estimationPoints);
+				if (taskss.get(j).estimationPointsPerDay.size() > i) {
+					estimationPointsPerDay[i] = estimationPointsPerDay[i]
+							+ taskss.get(j).getEffortPerDay(i);
 				}
 			}
 		}
@@ -281,28 +320,23 @@ public class Sprint extends SmartModel
 		String xTicks = "[";
 		String yTicks = "[]";
 
-		for( int i = 0; i <= estimationPointsPerDay.length; i++ )
-		{
-			if( i != estimationPointsPerDay.length )
-			{
-				if( i == estimationPointsPerDay.length - 1 )
-				{
+		for (int i = 0; i <= estimationPointsPerDay.length; i++) {
+			if (i != estimationPointsPerDay.length) {
+				if (i == estimationPointsPerDay.length - 1) {
 					m = m + "[" + (i) + "," + estimationPointsPerDay[i] + "]";
 
-				}
-				else
-				{
+				} else {
 					m = m + "[" + (i) + "," + estimationPointsPerDay[i] + "],";
 				}
 				xTicks = xTicks + (i) + ",";
-			}
-			else
+			} else
 				xTicks = xTicks + (i);
 
 		}
 		xTicks = xTicks + "]";
 		m = m + "]";
 		String k = ",";
-		return m + k + xTicks + k + yTicks + k + xLabel + k + yLabel + k + title + k + xMax + k + yMax;
+		return m + k + xTicks + k + yTicks + k + xLabel + k + yLabel + k
+				+ title + k + xMax + k + yMax;
 	}
 }
