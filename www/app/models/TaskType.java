74defd50c6dbbe105fa3c6d2248862c3b7f64d1a
amr.mohamed.abdelwahab <amr.mohamed.abdelwahab@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-08-08 07:58:06 +0000

    Documentation.

--- www/app/models/TaskType.java
+++ www/app/models/TaskType.java
@@ -15,22 +15,41 @@ import play.data.validation.Required;
 
 @Entity
 public class TaskType extends SmartModel {
-
+	/**
+	 * name
+	 */
 	@Required
-	@MaxSize (100)
+	@MaxSize(100)
 	public String name;
-
+	/**
+	 * the project it belongs to
+	 */
 	@ManyToOne
 	public Project project;
-
+	/**
+	 * if deleted=true
+	 */
 	public boolean deleted;
-
-	@OneToMany (mappedBy = "taskType", cascade = CascadeType.ALL)
+	/**
+	 * The list of tasks that have this type
+	 */
+	@OneToMany(mappedBy = "taskType", cascade = CascadeType.ALL)
 	public List<Task> Tasks;
+	/**
+	 * The color used to indicate the task type in the board.
+	 */
+	String hexColor = "#"
+			+ Integer.toHexString(new Color(new Random().nextInt(50) + 180,
+					new Random().nextInt(50) + 180,
+					new Random().nextInt(50) + 180).getRGB() & 0xFFFFFF);
 
-	String hexColor = "#" + Integer.toHexString(new Color(new Random().nextInt(50) + 180, new Random().nextInt(50) + 180, new Random().nextInt(50) + 180).getRGB() & 0xFFFFFF);
+	/**
+	 * Class constructor just initializing the list of tasks for this task type.
+	 * 
+	
+	 */
 
-	public TaskType () {
+	public TaskType() {
 		Tasks = new ArrayList<Task>();
 	}
 
