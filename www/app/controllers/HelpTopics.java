6a9383f30eb841872c73aada3423a14f12b1bdb8
Mahmoud Sakr <kamasheto@users.noreply.github.com>
2010-08-09 20:22:38 +0000

    Cleaned up code

--- www/app/controllers/HelpTopics.java
+++ www/app/controllers/HelpTopics.java
@@ -1,30 +1,33 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
 
-import java.util.*;
-
-import models.*;
+import models.HelpTopic;
 
 /**
  * Controller to handle and show all help topics
+ * 
  * @author ms
  */
 public class HelpTopics extends SmartController {
-	
+
 	/**
 	 * Shows all topics (per project)
-	 * @param projectId Project id, we might need this in the future if we're going to let project admins add topics per project
+	 * 
+	 * @param projectId
+	 *            Project id, we might need this in the future if we're going to
+	 *            let project admins add topics per project
 	 */
-    public static void topics(long projectId) {
+	public static void topics(long projectId) {
 		List<HelpTopic> topics = HelpTopic.findAll();
 		render(topics);
 	}
-	
+
 	/**
 	 * Shows this topic
-	 * @param id topic id
+	 * 
+	 * @param id
+	 *            topic id
 	 */
 	public static void topic(long id) {
 		HelpTopic topic = HelpTopic.findById(id);
--- www/app/models/HelpTopic.java
+++ www/app/models/HelpTopic.java
@@ -1,38 +1,38 @@
 package models;
 
-import play.*;
-import play.db.jpa.*;
-
-import javax.persistence.*;
-import java.util.*;
+import javax.persistence.Entity;
 
 /**
- * Help Topic Model. 
- * Holds the casts here for easier reference in the future.
+ * Help Topic Model. Holds the casts here for easier reference in the future.
  */
 @Entity
 public class HelpTopic extends SmartModel {
-	
+
 	/**
 	 * Summary of this help topic
 	 */
 	public String summary;
-	
+
 	/**
-	 * URL of this topic.. saved in case we need to include casts from external sources
+	 * URL of this topic.. saved in case we need to include casts from external
+	 * sources
 	 */
 	public String url;
-	
+
 	/**
 	 * Default constructor
-	 * @param summary Summary of this help topic
-	 * @param filename Filename of this cast, used for easier reference to our local casts
+	 * 
+	 * @param summary
+	 *            Summary of this help topic
+	 * @param filename
+	 *            Filename of this cast, used for easier reference to our local
+	 *            casts
 	 */
-	public HelpTopic(String summary, String filename) {
+	public HelpTopic (String summary, String filename) {
 		this.summary = summary;
 		this.url = "/public/screencasts/" + filename;
 	}
-	
+
 	/**
 	 * Static object of HelpTopic
 	 */
@@ -41,21 +41,23 @@ public class HelpTopic extends SmartModel {
 		 * Summary of this help topic
 		 */
 		public String summary;
-		
+
 		/**
 		 * filename of this help topic, for easier reference
 		 */
 		public String filename;
-		
+
 		/**
 		 * Default constructor
-		 * @param s Summary of this topic
-		 * @param f filename of this topic
+		 * 
+		 * @param s
+		 *            Summary of this topic
+		 * @param f
+		 *            filename of this topic
 		 */
-		public Object(String s, String f) {
+		public Object (String s, String f) {
 			summary = s;
 			filename = f;
 		}
 	}
 }
-
