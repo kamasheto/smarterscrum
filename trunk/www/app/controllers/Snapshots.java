0925d55b78ebcd595fab8fbfb6ced6d26c9650f5
dina.e.helal <dina.e.helal@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-08-10 08:49:45 +0000

    clean up code

--- trunk/www/app/controllers/Snapshots.java
+++ trunk/www/app/controllers/Snapshots.java
@@ -41,173 +41,134 @@ public class Snapshots extends SmartController {
 		Board b ;
 		List<Column> columns;
 		User user = Security.getConnected();
-		
-		if(componentID==0){
+		if(componentID==0)
 		b= p.board;
-				}
-		else{
-		Component c = Component.findById(componentID);
-		 b = c.componentBoard;
-			
+		else
+		{
+			Component c = Component.findById(componentID);
+			b = c.componentBoard;	
 		}
 		List<Column> CS =new ArrayList<Column>();
-		 columns = b.columns;
-			
+		columns = b.columns;	
 		ArrayList<ComponentRowh> data = new ArrayList<ComponentRowh>();
-	
 		ArrayList<String> Columnsofsnapshot = new ArrayList<String>();
 		for( int i=0; i<columns.size();i++)
 		{
 			if(columns.get( i ).onBoard==true)
-			{CS.add( null );
+			{
+				CS.add( null );
 				CS.set(i, columns.get( i ) );
 			}
-		}for( int i=0; i<columns.size();i++)
+		}
+		for( int i=0; i<columns.size();i++)
 		{
 			if(columns.get( i ).onBoard==true)
 			{
 				CS.set(columns.get(i).sequence, columns.get( i ) );
 			}
 		}
-		for (int i = 0; i < CS.size(); i++) {
+		for (int i = 0; i < CS.size(); i++) 
+		{
 			Columnsofsnapshot.add(null);
 			Columnsofsnapshot.set(i, CS.get(i).name);
 		}
-
-//		int smallest;
-//		Column temp;
-//		for (int i = 0; i < CS.size(); i++) {
-//			smallest = i;
-//			System.out.println(CS.get(i).name);
-//			System.out.println(CS.get(i).sequence);
-//			for (int j = i + 1; j < CS.size(); j++) {
-//				if (CS.get(smallest).sequence > CS.get(j).sequence) {
-//					smallest = j;
-//
-//				}
-//
-//			}
-//			temp = CS.get(smallest);
-//			CS.set(smallest, columns.get(i));
-//			CS.set(i, temp);
-//			Columnsofsnapshot.set(smallest, CS.get(i).name);
-//			Columnsofsnapshot.set(i, temp.name);}
-//		}for (int i = 0; i < CS.size(); i++) {
-//		System.out.println(CS.get(i).name);
-//		System.out.println(CS.get(i).sequence);}
-if(componentID==0){
-	List<Component> components = p.getComponents();
-	String type = p.name;
-	for (int i = 0; i < components.size(); i++)// for each component get
-		// the tasks
+		if(componentID==0)
 		{
-data.add(null);if(components.get(i).number!=0){
-			data.add(null);
-			data.set(i, new ComponentRowh(components.get(i).id, components.get(i).name));
-			List<Task> tasks = components.get(i).returnComponentTasks(s);
-
-			for (int j = 0; j < CS.size(); j++) {
-				data.get(i).add(null);
-				data.get(i).set(j, new ArrayList<String>());
-			}
-
-			for (Task task : tasks) {
-				Column pcol = new Column();
-				for(int k=0;k<task.taskStatus.columns.size();k++)
+			List<Component> components = p.getComponents();
+			String type = p.name;
+			for (int i = 0; i < components.size(); i++)// for each component get the tasks
+			{
+				data.add(null);
+				if(components.get(i).number!=0)
 				{
-					pcol = task.taskStatus.columns.get(k);
-					if(pcol.board.id==b.id)
+					data.add(null);
+					data.set(i, new ComponentRowh(components.get(i).id, components.get(i).name));
+					List<Task> tasks = components.get(i).returnComponentTasks(s);
+					for (int j = 0; j < CS.size(); j++) 
 					{
-						break;
+						data.get(i).add(null);
+						data.get(i).set(j, new ArrayList<String>());
 					}
-				}
-				if(pcol.onBoard==true&&!pcol.deleted)
-				{
-				data.get(i).get(pcol.sequence).add("(" + "T" + task.id + "-" + task.description + "-" + task.assignee.name);
-				}
-			}
-		}}	
-	
-	Snapshot snap = new Snapshot();
-	snap.user = user;
-	snap.type = type;
-	snap.board = b;
-	snap.sprint = s;
-	snap.data = data;
-	snap.Columnsofsnapshot = Columnsofsnapshot;
-	snap.save();
-	if(meetingID!=0){
-		Meeting M=Meeting.findById(meetingID);
-		M.snapshot=snap;
-		M.save();
-		snap.type =M.name+ " Meeting";
-		snap.save();
-		Calendar cal = new GregorianCalendar();
-		Logs.addLog(user, "Took", "Snapshot", snap.id, p, cal.getTime());
-		
-	}
-//	else{
-//	Calendar cal = new GregorianCalendar();
-//	Logs.addLog(user, "Took", "Snapshot", snap.id, p, cal.getTime());		}
-	
-
-}
-
-else{
-Component c = Component.findById(componentID);
-String type = c.name;
-List<User> users = c.getUsers();
-	for (int i = 0; i < users.size(); i++)
-	{
-		data.add(null);
-		data.set(i, new ComponentRowh(users.get(i).id, users.get(i).name));
-		List<Task> tasks = users.get(i).returnUserTasks(s, componentID);
-
-		for (int j = 0; j < CS.size(); j++) {
-			data.get(i).add(null);
-			data.get(i).set(j, new ArrayList<String>());
-		}
-
-		for (Task task : tasks) {
-			Column pcol = new Column();
-			for(int k=0;k<task.taskStatus.columns.size();k++)
+					for (Task task : tasks) 
+					{
+						Column pcol = new Column();
+						for(int k=0;k<task.taskStatus.columns.size();k++)
+						{
+							pcol = task.taskStatus.columns.get(k);
+							if(pcol.board.id==b.id)
+							break;
+						}
+						if(pcol.onBoard==true&&!pcol.deleted)
+						data.get(i).get(pcol.sequence).add("(" + "T" + task.id + "-" + task.description + "-" + task.assignee.name);
+					}
+				}		
+			}	
+			Snapshot snap = new Snapshot();
+			snap.user = user;
+			snap.type = type;
+			snap.board = b;
+			snap.sprint = s;
+			snap.data = data;
+			snap.Columnsofsnapshot = Columnsofsnapshot;
+			snap.save();
+			if(meetingID!=0)
 			{
-				pcol = task.taskStatus.columns.get(k);
-				if(pcol.board.id==b.id)
+				Meeting M=Meeting.findById(meetingID);
+				M.snapshot=snap;
+				M.save();
+				snap.type =M.name+ " Meeting";
+				snap.save();
+				Calendar cal = new GregorianCalendar();
+				Logs.addLog(user, "Took", "Snapshot", snap.id, p, cal.getTime());	
+			}
+		}
+		else
+		{
+			Component c = Component.findById(componentID);
+			String type = c.name;
+			List<User> users = c.getUsers();
+			for (int i = 0; i < users.size(); i++)
+			{
+				data.add(null);
+				data.set(i, new ComponentRowh(users.get(i).id, users.get(i).name));
+				List<Task> tasks = users.get(i).returnUserTasks(s, componentID);
+				for (int j = 0; j < CS.size(); j++) 
 				{
-					break;
+					data.get(i).add(null);
+					data.get(i).set(j, new ArrayList<String>());
+				}
+				for (Task task : tasks) 
+				{
+					Column pcol = new Column();
+					for(int k=0;k<task.taskStatus.columns.size();k++)
+					{
+						pcol = task.taskStatus.columns.get(k);
+						if(pcol.board.id==b.id)
+							break;			
+					}
+					data.get(i).get(CS.indexOf(pcol)).add( "T" + task.id + "-" + task.description + "-" + task.assignee.name);
 				}
 			}
-			if(pcol.onBoard==true&&!pcol.deleted)
+			Snapshot snap = new Snapshot();
+			snap.user = user;
+			snap.type = type;
+			snap.board = b;
+			snap.sprint = s;
+			snap.data = data;
+			snap.Columnsofsnapshot = Columnsofsnapshot;
+			snap.save();
+			if(meetingID!=0)
 			{
-			data.get(i).get(CS.indexOf(pcol)).add( "T" + task.id + "-" + task.description + "-" + task.assignee.name);
-			}
+				Meeting M=Meeting.findById(meetingID);
+				M.snapshot=snap;
+				M.save();
+				snap.type =M.name+ " Meeting";
+				snap.save();
+				Calendar cal = new GregorianCalendar();
+				Logs.addLog(user, "Took", "Snapshot", snap.id, p, cal.getTime());
+			}	
 		}
-	
 	}
-	Snapshot snap = new Snapshot();
-	snap.user = user;
-	snap.type = type;
-	snap.board = b;
-	snap.sprint = s;
-	snap.data = data;
-	snap.Columnsofsnapshot = Columnsofsnapshot;
-	snap.save();
-	if(meetingID!=0){
-		Meeting M=Meeting.findById(meetingID);
-		M.snapshot=snap;
-		M.save();
-		snap.type =M.name+ " Meeting";
-		snap.save();
-		Calendar cal = new GregorianCalendar();
-		Logs.addLog(user, "Took", "Snapshot", snap.id, p, cal.getTime());
-		
-	}
-
-	
-}
-
-		}
 
 
 	/**
