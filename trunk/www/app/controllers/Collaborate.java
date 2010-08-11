450f93229036cd504085e4ce962ce606fb820c2d
me@sakr.me <me@sakr.me@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-08-11 11:26:24 +0000

    Modified Collaborate to release poll when new users become online

--- trunk/www/app/controllers/Collaborate.java
+++ trunk/www/app/controllers/Collaborate.java
@@ -17,7 +17,7 @@ public class Collaborate extends SmartController {
 	/**
 	 * Renders a JSON object of all changes performed for this user (for dynamic udpate)
 	 */
-	public static void index(long lastUpdate, int currentlyOnline) {
+	public static void index(long lastUpdate, String currentlyOnline) {
 		CollaborateResponse response = new CollaborateResponse();
 		User user = Security.getConnected();
 		
@@ -57,16 +57,36 @@ public class Collaborate extends SmartController {
 		List<Session> sessions = Session.find( "order by lastClick desc" ).fetch();
 
 		List<User.Object> users = new ArrayList<User.Object>();
-		for( Session session : sessions )
-		{
-			users.add( new User.Object( session.user.id, session.user.name, session.lastClick, session.user.isAdmin ) );
+		for (Session session : sessions) {
+			users.add(new User.Object(session.user.id, session.user.name, session.lastClick, session.user.isAdmin));
 		}
 		response.online_users = users;
+		
+		String[] idsStr = currentlyOnline.split(",");
+		boolean newUsers = idsStr.length != users.size(); // true if they're not equal, false otherwise
+		// remember we'll take that a step further by confirming they're not the same.. next
+		
+		outerLoop : for (String idStr : idsStr) {
+			if (idStr.length() < 1)
+				continue;
+			long id = Long.parseLong(idStr);
+			boolean contains = false;
+			innerLoop : for (User.Object usrObj : users) {
+				if (usrObj.id == id) {
+					contains = true;
+					break innerLoop;
+				}
+			}
+			newUsers |= !contains;
+			if (newUsers) {
+				break outerLoop;
+			}
+		}
 		
 		/**
 		 * Suspend 
 		 */
-		if (response.news.isEmpty() && response.updates.isEmpty() && response.online_users.size() == currentlyOnline) {
+		if (response.news.isEmpty() && response.updates.isEmpty() && !newUsers) {
 			suspend("1s");
 		}
 		
