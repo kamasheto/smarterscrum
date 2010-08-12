35c6d958d46abe54a8093253fc5a1bee1c0820ee
iistcrimi@gmail.com <iistcrimi@gmail.com@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-08-12 12:35:29 +0000

    the sprint backlog is now collaborative, and the inplace editing checks was modidfed

--- trunk/www/public/javascripts/requirements.js
+++ trunk/www/public/javascripts/requirements.js
@@ -22,3 +22,25 @@ function parent_message_bar(message) {
 		message: message
 		});
 }
+
+function sprintLoad(el)
+{
+	var url = $('#theLoadedContent').contents().find('#sprintTable #task_'+el).first().attr('name');
+	alert(url);
+	$('#theLoadedContent').contents().find('body:first').append('<div id="sprintTemp" style="display:none"><div/>');
+	$('#theLoadedContent').contents().find('#sprintTemp:first').load(url+' #sprintTable #task_'+el,function(){
+		$('#theLoadedContent').contents().find('#sprintTable #task_'+el).first().replaceWith($('#theLoadedContent').contents().find('#sprintTemp:first').html());
+		$('#theLoadedContent').contents().find('#sprintTemp:first').remove();
+		$('#theLoadedContent').contents().find(".enterEffort").each(function()
+				{
+					if($(this).html()!='')
+						$(this).html(parseInt($(this).html()));
+				});
+
+		$('#theLoadedContent').contents().find(".editPOINTS").each(function()
+				{
+					if($(this).html()!='')
+						$(this).html(parseInt($(this).html()));
+				});
+	});
+}
