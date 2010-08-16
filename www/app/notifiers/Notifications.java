20c58955fac518b127b5376acefc1d2ea7b10e47
moataz.mekki <moataz.mekki@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-08-16 20:14:03 +0000

    update issue 233 Status: Pending added the documentation for the notifications :X

--- www/app/notifiers/Notifications.java
+++ www/app/notifiers/Notifications.java
@@ -10,6 +10,14 @@ import controllers.Security;
 
 public class Notifications extends Mailer{
 
+	/**
+	 * sends an email to a user in order to activate his account or his new email address
+	 * that he requested to change
+	 * @param userEmail: the new email or the email he registered on
+	 * @param userName: name of the user to activate
+	 * @param url: the url that activates the account which contains the hash
+	 * @param changeEmail: to differentiate between first activation on registration and changing his email address 
+	 */
 	public static void activate(String userEmail, String userName, String url, boolean changeEmail)
 	{
 		addRecipient(userEmail);
@@ -18,6 +26,16 @@ public class Notifications extends Mailer{
 		send(userName, url, changeEmail);
 	}
 	
+	/**
+	 * sends an email when the user is invited to a project or ameeting
+	 * @param user: the invied user
+	 * @param objectURL: the Url of the meeting or project
+	 * @param objectName: the name of the meeting (only meeting)
+	 * @param confirmURL: the url that enables the user to confirm the invitation
+	 * @param declineURL: the url that enables the user to decline the invitation
+	 * @param project: the project of the meeting , or the project the user invited to
+	 * @param meeting: to check if it's an invitation for a meeting or a project
+	 */
 	public static void invite(User user, String objectURL, String objectName, String confirmURL, String declineURL, Project project, boolean meeting)
 	{
 		User actionPerformer = Security.getConnected();
@@ -41,6 +59,11 @@ public class Notifications extends Mailer{
 		}
 	}
 	
+	/**
+	 * sends an email to the user who forgot his password with a url to change it
+	 * @param user: the user who forgot his password & will receive this email
+	 * @param url: the url to change the password
+	 */
 	public static void lostPass(User user, String url)
 	{
 		setFrom("se.smartsoft.2@gmail.com");
@@ -49,6 +72,11 @@ public class Notifications extends Mailer{
 		send(user, url);
 	}
 	
+	/**
+	 * sends an email to welcome a new registered user or to confirm changing the email address of a user successfully
+	 * @param user: the receiver of this email
+	 * @param welcome: to check is it a welcome message or just a confirmations email
+	 */
 	public static void welcome(User user, boolean welcome)
 	{
 		setFrom("se.smartsoft.2@gmail.com");
@@ -57,6 +85,11 @@ public class Notifications extends Mailer{
 		send(user, welcome);
 	}
 	
+	/**
+	 * sends an email to tell the user that he's been deleted from the system
+	 * @param user: the deleted user
+	 * @param request: to check if the user requested to be deleted or he was deleted by a system admin
+	 */
 	public static void byeBye(User user, boolean request)
 	{
 		setFrom("se.smartsoft.2@gmail.com");
@@ -65,6 +98,15 @@ public class Notifications extends Mailer{
 		send(user, request);
 	}
 	
+	/**
+	 * sends notifications for specific actions to all the project members
+	 * @param project: the project that its users will be notified
+	 * @param actionType: the boolean variable in the projectNotificationProfile model to check if it's set to true or false
+	 * @param resourceURL: the resource that the action was done upon
+	 * @param resourceType: the type of that resource
+	 * @param resourceName: the name of that resource
+	 * @param importance: 1 for acceptance, 0 for announcements, -1 for deletion
+	 */
 	public static void notifyProjectUsers(Project project , String actionType, String resourceURL, String resourceType, String resourceName, byte importance)
 	{
 		User user = Security.getConnected();
@@ -123,6 +165,16 @@ public class Notifications extends Mailer{
 		}
 	}
 	
+	/**
+	 * sends a notification to a list of users who are involved with the resource
+	 * @param receivers: the users who will receive this notification
+	 * @param actionType: the type of the action (add, delete, create, remove, edit ...)
+	 * @param resourceURL: the resource that the action was done upon
+	 * @param resourceType: the type of that resource
+	 * @param resourceName: the name of that resource
+	 * @param importance: 1 for acceptance, 0 for announcements, -1 for deletion
+	 * @param project: the project that this action took place in
+	 */
 	public static void notifyUsers(List<User> receivers, String actionType, String resourceURL, String resourceType, String resourceName, byte importance, Project project)
 	{		
 		User user = Security.getConnected();
@@ -145,6 +197,16 @@ public class Notifications extends Mailer{
 		send(user, actionType, resourceURL, resourceType, resourceName, importance, project);
 	}
 	
+	/**
+	 * sends a notification to only one user
+	 * @param receiver: the user who will receive this notification
+	 * @param actionType: the type of the action (add, delete, create, remove, edit ...)
+	 * @param resourceURL: the resource that the action was done upon
+	 * @param resourceType: the type of that resource
+	 * @param resourceName: the name of that resource
+	 * @param importance: 1 for acceptance, 0 for announcements, -1 for deletion
+	 * @param project: the project that this action took place in
+	 */
 	public static void notifyUser(User receiver, String actionType, String resourceURL, String resourceType, String resourceName, byte importance, Project project)
 	{		
 		User usr = Security.getConnected();
@@ -166,6 +228,13 @@ public class Notifications extends Mailer{
 		}
 	}
 	
+	/**
+	 * sends an email to the support team when a user gives a feedback on the system
+	 * @param sender: the user giving the feedback
+	 * @param subject: the summary of this feedback
+	 * @param message: the detailed description of the feedback
+	 * @param priority: the priority of the feedback specified by the user
+	 */
 	public static void feedbacks(User sender, String subject, String message, int priority)
 	{
 		String importance="";
