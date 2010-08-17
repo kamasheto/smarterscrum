8a16a62aa4e1b338dbf7bc4a0c7878bf5ac63b1b
galalaly28@gmail.com <galalaly28@gmail.com@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-08-17 09:13:34 +0000

    Documented

--- trunk/www/app/controllers/Feedbacks.java
+++ trunk/www/app/controllers/Feedbacks.java
@@ -11,11 +11,19 @@ import play.mvc.With;
 
 @With( Secure.class )
 public class Feedbacks extends SmartController{
-	
+	/**
+	 * renders the form
+	 */
 	public static void index(){
 		render();
 	}
 	
+	/**
+	 * sends the feedback to the email of the support people.
+	 * @param code the user input for the captcha image (for validation)
+	 * @param title title/subject of the email to be sent (usually a bug summary)
+	 * @param description detailed description of the bug/suggestion
+	 */
 	public static void sendFeedback(String code, String title, String description){
 		validation.required(title);
 		validation.required(description);
@@ -34,6 +42,10 @@ public class Feedbacks extends SmartController{
 		}
 	}
 	
+	/**
+	 * Generates the captcha image to validate the user input 
+	 * (called from the view of the index)
+	 */
 	public static void captcha(){
 		Images.Captcha captcha = Images.captcha();
 		String code = captcha.getText("#000000");
