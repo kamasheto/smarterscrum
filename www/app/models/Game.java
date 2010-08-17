d9949c1aab29b5e86cab9f3ae35955dd405bf2f4
mohamed.monayri <mohamed.monayri@gmail.com>
2010-08-17 13:13:20 +0000

    Updating the game controllers and models

--- www/app/models/Game.java
+++ www/app/models/Game.java
@@ -1,59 +1,59 @@
-// package models;
-//
-// import java.util.ArrayList;
-// import java.util.Collections;
-// import java.util.List;
-//
-// import javax.persistence.Entity;
-// import javax.persistence.ManyToMany;
-// import javax.persistence.ManyToOne;
-//
-// @Entity
-// public class Game extends SmartModel
-// {
-// //
-// // @OneToMany
-// // public List<Round> rounds;
-//
-// @ManyToMany
-// public List<Task> stories;
-//
-// @ManyToOne
-// public Component component;
-//
-// @ManyToOne
-// public Task currentStory;
-//
-// public ChatRoom chatroom;
-//
-// public Game()
-// {
-// stories = new ArrayList<Task>();
-// }
-//
-// public void init()
-// {
-//
-// chatroom = new ChatRoom( component.project ).save();
-// this.save();
-// }
-//
-// public Round getRound()
-// {
-// List<Round> rounds = Round.find( "game = ? order by id desc", this ).fetch();
-// if( rounds.isEmpty() )
-// {
-// return null;
-// }
-// return rounds.get( 0 );
-// }
-//
-// public User getModerator()
-// {
-//
-// List<GameSession> sessionsInGame = GameSession.find( "byGame", this
-// ).fetch();
-// Collections.sort( sessionsInGame );
-// return sessionsInGame.get( 0 ).user;
-// }
-// }
+ package models;
+
+ import java.util.ArrayList;
+ import java.util.Collections;
+ import java.util.List;
+
+ import javax.persistence.Entity;
+ import javax.persistence.ManyToMany;
+ import javax.persistence.ManyToOne;
+
+ @Entity
+ public class Game extends SmartModel
+ {
+ //
+ // @OneToMany
+ // public List<Round> rounds;
+
+ @ManyToMany
+ public List<Task> tasks;
+
+ @ManyToOne
+ public Component component;
+
+ @ManyToOne
+ public Task currentTask;
+
+ public ChatRoom chatroom;
+
+ public Game()
+ {
+ tasks = new ArrayList<Task>();
+ }
+
+ public void init()
+ {
+
+ chatroom = new ChatRoom( component.project ).save();
+ this.save();
+ }
+
+ public Round getRound()
+ {
+ List<Round> rounds = Round.find( "game = ? order by id desc", this ).fetch();
+ if( rounds.isEmpty() )
+ {
+ return null;
+ }
+ return rounds.get( 0 );
+ }
+
+ public User getModerator()
+ {
+
+ List<GameSession> sessionsInGame = GameSession.find( "byGame", this
+ ).fetch();
+ Collections.sort( sessionsInGame );
+ return sessionsInGame.get( 0 ).user;
+ }
+ }
--- www/app/models/Trick.java
+++ www/app/models/Trick.java
@@ -1,16 +1,16 @@
-// package models;
-//
-// import javax.persistence.Entity;
-// import javax.persistence.ManyToOne;
-//
-// @Entity
-// public class Trick extends SmartModel {
-//
-// @ManyToOne
-// public User user;
-//
-// public double estimate;
-//
-// @ManyToOne
-// public Round round;
-// }
+ package models;
+
+ import javax.persistence.Entity;
+ import javax.persistence.ManyToOne;
+
+ @Entity
+ public class Trick extends SmartModel {
+
+ @ManyToOne
+ public User user;
+
+ public double estimate;
+
+ @ManyToOne
+ public Round round;
+ }
