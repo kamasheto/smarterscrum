55962d1779d99a7be5b533494667f48a75b5215d
justheba <justheba@e4276f5f-cf7c-4f52-70db-af008640ad72>
2010-08-18 08:55:22 +0000

    uploader aint completed yet

--- www/app/controllers/ProjectTasks.java
+++ www/app/controllers/ProjectTasks.java
@@ -64,7 +64,7 @@ public class ProjectTasks extends SmartController {
 		if((!role.baseRole)&&((connectedUser.in(role.project).can("revokeUserRole"))||(user.id == connectedUser.id)))
 		{
 			user.removeRole(role, user);
-			msg="You have revoked a role successfuly|reload('roles', 'project-'+projectId+'-in-user-'+userId);";			
+			msg="You have revoked a role successfuly|reload('roles', 'project-'+projectId+'-in-user-'+userId, 'projects-in-user-'+userId);";			
 			String url = Router.getFullUrl("Application.externalOpen")+"?id="+role.project.id+"&isOverlay=false&url=/users/listUserProjects?userId="+user.id+"&boxId=2&projectId="+role.project.id+"&currentProjectId="+role.project.id;			
 			Notifications.notifyUser(user, "revoked", url, "your role", role.name, (byte)-1, role.project);
 			renderText(msg);
@@ -75,12 +75,12 @@ public class ProjectTasks extends SmartController {
 			if((connectedUser.in(role.project).can("revokeUserRole")))
 			{
 				user.removeRole(role, user);
-				msg="You have revoked a role successfuly, The user is no longer a member in this project.|reload('roles', 'users', 'projects-in-user-'+userId, 'user-'+userId, 'project-'+projectId+'-in-user-'+userId);|$('#project-search-result-'+projectId).remove();";
+				msg="You have revoked a role successfuly, The user is no longer a member in Project '" + role.project.name + "' .|reload('roles', 'users', 'projects-in-user-'+userId, 'user-'+userId, 'project-'+projectId+'-in-user-'+userId);$('#project-search-result-'+projectId).remove();";
 				String url = Router.getFullUrl("Application.externalOpen")+"?id="+role.project.id+"&isOverlay=false&url=#";
 				Notifications.notifyUser(user, "deleted", url, "you from project", role.project.name, (byte)-1, null);
 				renderText(msg);
 			}
-			else if((user.id == connectedUser.id))
+			else if((user == connectedUser))
 			{
 				msg="You cannot revoke this role without requesting to be deleted from this project.";
 				renderText(msg);
