bc0969c8ca59e63d28e2212896a77bc09ea9232b
galalaly28 <galalaly28@gmail.com>
2010-08-18 12:06:57 +0000

    Removed comment + ignored bootstrap problem

--- www/app/models/Task.java
+++ www/app/models/Task.java
@@ -169,7 +169,7 @@ public class Task extends SmartModel
 		{
 			this.project = this.parent.project;
 			this.component = this.parent.component;
-			this.number = this.parent.subTasks.size() +1;//kan fee +1 hena shelto 3ashan el subtasks numbering system yetzebet - El Sanfoor
+			this.number = this.parent.subTasks.size() +1;
 			for( Task task : this.parent.subTasks )
 			{
 				if( task.number >= this.number && !this.equals( task ) )
