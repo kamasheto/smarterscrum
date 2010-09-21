f35650e61b3de5d3c6e312b70659a3886eebeba6
Mahmoud Sakr <kamasheto@users.noreply.github.com>
2010-09-21 00:05:30 +0000

    Documented Dummy class

--- www/app/controllers/Dummy.java
+++ www/app/controllers/Dummy.java
@@ -7,10 +7,19 @@ import java.util.*;
 
 import models.*;
 
+/**
+ * Dummy class for dummy actions
+ * MUST BE REMOVED ON A PRODUCTION ENVIRONMENT
+ * To access CRUD forms: http://url/admin/dummy
+ * To access normal actions: http://url/dummy/action
+ */
 @CRUD.For(User.class)
 public class Dummy extends SmartCRUD {
 	/**
-	 * Dummy action that promotes the give user to admin
+	 * Dummy action that promotes the give user to admin (or not)
+	 * @param id user id to promote
+	 * @param isAdmin whether to give this user admin permissions or not
+	 * @deprecated use CRUD form instead
 	 */
 	public static void promote(long id, boolean isAdmin) {
 		if (id == 0) {
