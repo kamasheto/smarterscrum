d99e16e184bdafc81286af1a990f2701a2d6334f
Mahmoud Sakr <kamasheto@users.noreply.github.com>
2010-09-26 13:26:36 +0000

    Fixed inconsistency with calculating Collaborative updates

--- www/public/javascripts/collaborative.js
+++ www/public/javascripts/collaborative.js
@@ -1,20 +1,20 @@
 last_update = new Date().getTime();
 var ping = function() {
-	$.getJSON('/collaborate?lastUpdate=' + last_update + '&currentlyOnline=' + get_online_users(), function(resp) {
-		last_update = new Date().getTime()
+	$.getJSON('/collaborate?lastUpdate=' + last_update + '&currentlyOnline=' + get_online_users(), function(response) {
+		last_update = response.last_update
 		
-		$(resp.news).each(function(){
-			// $.gritter.add({
-			// 	title: 'New Notification',
-			// 		text: this.actionPerformer.name +' has '+this.actionType+'ed'+ this.resourceType+': <a href = '+this.resourceURL+'>'+this.resourceName+'</a>',
-			// 	image: this.importance > 0 ? '/public/images/tick.png' : this.importance < 0 ? '/public/images/cross.png' : '/public/images/error.png',
-			// 	sticky: false,
-			// 	time: '10000'
-			// })
-		})
+		// $(response.news).each(function(){
+		// 	$.gritter.add({
+		// 		title: 'New Notification',
+		// 			text: this.actionPerformer.name +' has '+this.actionType+'ed'+ this.resourceType+': <a href = '+this.resourceURL+'>'+this.resourceName+'</a>',
+		// 		image: this.importance > 0 ? '/public/images/tick.png' : this.importance < 0 ? '/public/images/cross.png' : '/public/images/error.png',
+		// 		sticky: false,
+		// 		time: '10000'
+		// 	})
+		// })
 		
 		str = '';
-		$(resp.online_users).each(function() {
+		$(response.online_users).each(function() {
 			isAdmin = this.isAdmin ? 'systemAdmin' : '';
 			str += '<span class="online-user '+isAdmin+'" user_id="'+this.id+'">' + this.name + '</span>, ';
 		});
@@ -22,7 +22,7 @@ var ping = function() {
 		$('#online_users_list').html(str.substring(0,str.length-2));
 		
 		
-		$(resp.updates).each(function() {
+		$(response.updates).each(function() {
 			eval(this.javascript)
 		})
 		
