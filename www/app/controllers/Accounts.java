2ed908c38ce0a12548a4e9eb3c5efe357e4e94a1
daliaMokhtar91 <daliaMokhtar91@gmail.com>
2011-08-08 19:43:53 +0000

    tiny bit of validation added

--- www/app/controllers/Accounts.java
+++ www/app/controllers/Accounts.java
@@ -71,6 +71,7 @@ public class Accounts extends SmartController {
 							if (mobile.length() != 0) {
 								there = true;
 								try {
+									mob = Integer.parseInt(mobile);
 									String temp0 =""+ mobile.charAt(0);
 									String temp1 =""+ mobile.charAt(1);
 									if (Integer.parseInt(temp0) != 0 || Integer.parseInt(temp1) != 1 || mobile.length()<10 || mobile.length()>11) {
